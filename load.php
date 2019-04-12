218a9f18aa80249fa14b8905c6bd755b340065fa
Robert O'Rourke <robert@o-rourke.org>
2019-04-12 11:39:56 +0100

    add default settings for workflows

--- load.php
+++ load.php
@@ -14,6 +14,8 @@ require_once __DIR__ . '/inc/namespace.php';
 add_action( 'hm-platform.modules.init', function () {
 	$default_settings = [
 		'enabled' => false,
+		'posts-workflow' => true,
+		'editorial-workflow' => true,
 	];
 	register_module( 'workflow', __DIR__, 'Workflow', $default_settings, __NAMESPACE__ . '\\bootstrap' );
 } );
