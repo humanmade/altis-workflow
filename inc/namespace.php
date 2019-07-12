c8517c87063b9b2f879c8be14b77b787b9eed454
Ryan McCue <me@ryanmccue.info>
2019-07-12 11:42:50 +0100

    Add Publication Checklist feature

--- inc/namespace.php
+++ inc/namespace.php
@@ -8,11 +8,36 @@
 namespace Altis\Workflow;
 
 use const Altis\ROOT_DIR;
+use function Altis\get_config;
 
 function bootstrap() {
 	add_action( 'muplugins_loaded', __NAMESPACE__ . '\\load_workflows', 0 );
+	add_action( 'muplugins_loaded', __NAMESPACE__ . '\\load_publication_checklist', 0 );
 }
 
 function load_workflows() {
 	require_once ROOT_DIR . '/vendor/humanmade/workflows/plugin.php';
+}
+
+/**
+ * Load the Publication Checklist feature, if enabled.
+ */
+function load_publication_checklist() {
+	$config = get_config()['modules']['workflow']['publication-checklist'] ?? null;
+	if ( ! $config ) {
+		return;
+	}
+
+	if ( ! is_array( $config ) ) {
+		$config = [];
+	}
+
+	if ( $config['block_publish'] ?? false ) {
+		add_filter( 'altis.publication-checklist.block_on_failing', '__return_true' );
+	}
+	if ( $config['hide_column'] ?? false ) {
+		add_filter( 'altis.publication-checklist.show_tasks_column', '__return_false' );
+	}
+
+	require_once ROOT_DIR . '/vendor/humanmade/publication-checklist/plugin.php';
 }
