48e110b4f0dc67ebe54e684b5a4dd4c431467963
Chris Reynolds <chris@jazzsequence.com>
2021-05-21 15:40:40 -0600

    load all the things
    this would normally be handled internally by a composer autoloader, but we can't use or rely on internal composer dependencies so we need to do it manually

--- inc/namespace.php
+++ inc/namespace.php
@@ -63,7 +63,46 @@ function load_duplicate_posts() {
 	if ( ! $config ) {
 		return;
 	}
+
+	// Load the main plugin file.
 	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/duplicate-post.php';
+
+	// Load all the other plugin files. This is normally handled internally by a Composer autoloader.
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/admin/options.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/admin/options-form-generator.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/admin/options-inputs.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/admin/options-page.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/duplicate-post.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/handlers/bulk-handler.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/handlers/check-changes-handler.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/handlers/handler.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/handlers/link-handler.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/handlers/save-post-handler.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/permissions-helper.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/post-duplicator.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/post-republisher.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/revisions-migrator.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/admin-bar.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/asset-manager.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/block-editor.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/bulk-actions.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/classic-editor.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/column.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/link-builder.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/metabox.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/post-states.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/row-actions.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/ui/user-interface.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/utils.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/watchers/bulk-actions-watcher.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/watchers/copied-post-watcher.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/watchers/link-actions-watcher.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/watchers/original-post-watcher.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/watchers/republished-post-watcher.php';
+	require_once Altis\ROOT_DIR . '/vendor/yoast/duplicate-post/src/watchers/watchers.php';
+
+	// Kick off the main Duplicate Post plugin class.
+	new \Yoast\WP\Duplicate_Post\Duplicate_Post();
 }
 
 /**
