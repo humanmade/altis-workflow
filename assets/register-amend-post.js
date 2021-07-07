211c7575550ca3abe92cda92b7a648a944c55a22
Chris Reynolds <chris@jazzsequence.com>
2021-07-07 15:29:28 -0600

    split javascript into two files
    one is for rewrite and republish posts and the other is for everything. if we're not on a rewrite and republish post, we'll bail out before we get to the rewrite & republish-specific stuff.

---
+++ assets/register-amend-post.js
@@ -0,0 +1,39 @@
+import { registerPlugin, unregisterPlugin } from "@wordpress/plugins";
+import { PluginPostStatusInfo } from "@wordpress/edit-post";
+import { Fragment } from "@wordpress/element";
+import { Button } from '@wordpress/components';
+import { select } from "@wordpress/data";
+
+unregisterPlugin( 'duplicate-post' );
+registerPlugin( 'altis-amend-post', {
+	render: () => {
+		const currentPostStatus = select( 'core/editor' ).getEditedPostAttribute( 'status' );
+
+		return (
+			<Fragment>
+				{ ( altisAmendPost.newDraftLink !== '' ) &&
+					<PluginPostStatusInfo>
+						<Button
+							isTertiary={ true }
+							className="dp-editor-post-copy-to-draft"
+							href={ altisAmendPost.newDraftLink }
+						>
+							{ altisAmendPost.clonePost }
+						</Button>
+					</PluginPostStatusInfo>
+				}
+				{ ( currentPostStatus === 'publish' && altisAmendPost.amendLink !== '' ) &&
+					<PluginPostStatusInfo>
+						<Button
+							isTertiary={ true }
+							className="dp-editor-post-rewrite-republish"
+							href={ altisAmendPost.amendLink }
+						>
+							{ altisAmendPost.amendPost }
+						</Button>
+					</PluginPostStatusInfo>
+				}
+			</Fragment>
+		);
+	}
+} );
---
+++ assets/republish-strings.js
@@ -0,0 +1,21 @@
+import { setLocaleData } from "@wordpress/i18n";
+const republishStrings = {
+	'Publish': altisRepublishStrings.publish,
+	'Publish:': altisRepublishStrings.publishColon,
+	'Publish on:': altisRepublishStrings.publishOn,
+	'Are you ready to publish?': altisRepublishStrings.readyToPublish,
+	'Schedule': altisRepublishStrings.scedule,
+	'Schedule…': altisRepublishStrings.scheduleEllipses,
+	'post action/button label\u0004Schedule': altisRepublishStrings.schedule,
+	'Are you ready to schedule?': altisRepublishStrings.readyToSchedule,
+	'is now scheduled. It will go live on': altisRepublishStrings.nowScheduled,
+};
+
+for ( const original in republishStrings ) {
+	setLocaleData( {
+		[ original ]: [
+			republishStrings[ original ],
+			'duplicate-post'
+		]
+	} );
+}
