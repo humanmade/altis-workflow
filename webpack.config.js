fc09f0672becb62f027d6fce1f16c221c723f2d8
Chris Reynolds <chris@jazzsequence.com>
2021-07-09 15:11:09 -0600

    use single quotes
    # Multiline description of commit
    # feel free to be detailed
    #
    # Ticket: #

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const { filePath } = helpers;
 module.exports = presets.production( {
 	externals,
 	entry: {
-		"republish-strings": filePath( 'assets/republish-strings.js' ),
-		"register-amend-post": filePath( 'assets/register-amend-post.js' ),
+		'republish-strings': filePath( 'assets/republish-strings.js' ),
+		'register-amend-post': filePath( 'assets/register-amend-post.js' ),
 	},
 	output: {
 		path: filePath( 'build' ),
