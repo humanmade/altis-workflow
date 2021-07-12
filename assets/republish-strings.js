d00b1f90ef99b404760917b20fdcd4ba410cd277
Chris Reynolds <chris@jazzsequence.com>
2021-07-12 12:40:57 -0600

    copy pasta wholesale javascript functions from duplicate-post
    many of these functions are not exported, so we can't import them. even if they are exported, their package doesn't include the original, unbuilt javascript files, and the compiled versions have version suffixes added to them, which would lock us into specific versions of the plugin. This allows us to not have to lock Duplicate Post at a specific version and update that version when each new release is made, we just need to watch for bugs/changes.

--- assets/republish-strings.js
+++ assets/republish-strings.js
@@ -1,5 +1,87 @@
 /* global altisRepublishStrings */
 import { setLocaleData } from '@wordpress/i18n';
+import { createInterpolateElement } from '@wordpress/element';
+import { Button } from '@wordpress/components';
+import { dispatch, select, subscribe } from "@wordpress/data";
+
+/**
+ * Saves the current post and redirects to the revision screen.
+ *
+ * This is copy pasta of an unexported function in duplicate-post/js/src/duplicate-post-strings.js.
+ */
+const saveAndCompare = () => {
+	dispatch( 'core/editor' ).savePost();
+
+	let wasSavingPost      = false;
+	let wasSavingMetaboxes = false;
+	let wasAutoSavingPost  = false;
+
+	/**
+	 * Determines when the redirect needs to happen.
+	 *
+	 * @returns {void}
+	 */
+	subscribe( () => {
+		const completed = redirectOnSaveCompletion( altisRepublishStrings.checkLink, { wasSavingPost, wasSavingMetaboxes, wasAutoSavingPost } );
+
+		wasSavingPost      = completed.isSavingPost;
+		wasSavingMetaboxes = completed.isSavingMetaBoxes;
+		wasAutoSavingPost  = completed.isAutosavingPost;
+	} );
+}
+
+/**
+ * Redirects to url when saving in the block editor has completed.
+ *
+ * This is copy pasta of a function in duplicate-post/js/src/duplicate-post-functions.js. While this function is exported, using the exported function would require locking Duplicate Post to a specific version since the uncompiled files are not part of the package.
+ *
+ * @param {string} url         The url to redirect to.
+ * @param {Object} editorState The current editor state regarding saving the post, metaboxes and autosaving.
+ *
+ * @returns {Object} The updated editor state.
+ */
+const redirectOnSaveCompletion = ( url, editorState ) => {
+	const isSavingPost       = select( 'core/editor' ).isSavingPost();
+	const isAutosavingPost   = select( 'core/editor' ).isAutosavingPost();
+	const hasActiveMetaBoxes = select( 'core/edit-post' ).hasMetaBoxes();
+	const isSavingMetaBoxes  = select( 'core/edit-post' ).isSavingMetaBoxes();
+
+	// When there are custom meta boxes, redirect after they're saved.
+	if ( hasActiveMetaBoxes && ! isSavingMetaBoxes && editorState.wasSavingMetaboxes ) {
+		redirectWithoutWarning( url );
+	}
+
+	// When there are no custom meta boxes, redirect after the post is saved.
+	if ( ! hasActiveMetaBoxes && ! isSavingPost && editorState.wasSavingPost && ! editorState.wasAutoSavingPost ) {
+		redirectWithoutWarning( url );
+	}
+
+	return { isSavingPost, isSavingMetaBoxes, isAutosavingPost };
+};
+
+/**
+ * This redirects without showing the warning that occurs due to a Gutenberg bug.
+ *
+ * Edits made to the post on the PHP side are not correctly recognized and thus the warning for unsaved changes is shown.
+ * By updating the post status ourselves on the JS side as well we avoid this.
+ *
+ * This is copy pasta of an unexported function in duplicate-post/js/src/duplicate-post-functions.js.
+ *
+ * @param {string} url The url to redirect to.
+ *
+ * @returns {void}
+ */
+ const redirectWithoutWarning = ( url ) => {
+	const currentPostStatus = select( 'core/editor' ).getCurrentPostAttribute( 'status' );
+	const editedPostStatus  = select( 'core/editor' ).getEditedPostAttribute( 'status' );
+
+	if ( currentPostStatus === 'dp-rewrite-republish' && editedPostStatus === 'publish' ) {
+		dispatch( 'core/editor' ).editPost( { status: currentPostStatus } );
+	}
+
+	window.location.assign( url );
+}
+
 const republishStrings = {
 	'Publish': altisRepublishStrings.publish,
 	'Publish:': altisRepublishStrings.publishColon,
